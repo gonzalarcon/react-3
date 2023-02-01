3de5d0d57e407717092aaf2f8e27e176e80a242e
Gonza <goialarcon@gmail.com>
2023-02-01 15:41:37 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { BaseColaboradores } from "./components/Data.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+
+const [users, setUsers] = useState([...BaseColaboradores]);
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+  });
+  const [filter, setFilter] = useState('');
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsers([...users, user]);
+    setUser({ name: '', email: '' });
+  };
+
+  const filteredUsers = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(filter.toLowerCase()) ||
+      user.email.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <div className="Filtro">
+          <h3>Buscador de Colaboradores</h3>
+          <input
+        type="text"
+        placeholder="Filtra por nombre o correo"
+        value={filter}
+        onChange={(event) => setFilter(event.target.value)}
+      />
+        </div>
+        <div className="inputs">
+          <form onSubmit={handleSubmit}>
+            <div className="input user">
+              <label>Nombre del Colaborador</label>
+              <input
+                type="text"
+                name="name"
+                value={user.name}
+                onChange={handleChange}
+                placeholder="Ingresa el nombre del Colaborador"
+              />
+            </div>
+            <div className="input user">
+              <label> Correo del Colaborador</label>
+              <input
+                type="email"
+                name="email"
+                value={user.email}
+                onChange={handleChange}
+                placeholder="Correo del Colaborador"
+              />
+            </div>
+            <button type="submit">Agregar Colaborador</button>
+          </form>
+        </div>
+        <div className="List">
+          <h2>Listado de Colaboradores</h2>
+          <ul>
+            {filteredUsers.map((user, index) => (
+              <li key={index}>
+                {user.name} ({user.email})
+              </li>
+            ))}
+          </ul>
+        </div>
+    </div>  
   );
 }
 
